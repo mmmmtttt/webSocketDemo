d8f865b2259d1cd423f4c55b47c8c25a87dd4d36
mengtong <mf120511@163.com>
2022-03-29 21:07:50 +0800

    finish

---
+++ frontend/app.js
@@ -0,0 +1,40 @@
+const socket = io('ws://localhost:8080');
+
+socket.on('connect', text => {
+    console.log("onconnected");
+    const el = document.createElement('li');
+    el.innerHTML = "ip: "+ text+" connected";
+    document.querySelector('ul').appendChild(el)
+});
+
+socket.on('message', text => {
+    const el = document.createElement('li');
+    el.innerHTML = "new message:" + text;
+    document.querySelector('ul').appendChild(el)
+});
+
+socket.on('disconnect', text => {
+    console.log("ondisconnect");
+    const el = document.createElement('li');
+    el.innerHTML = "ip: "+text+" disconnected";
+    document.querySelector('ul').appendChild(el)
+
+});
+
+document.querySelector('button').onclick = () => {
+    const text = document.querySelector('input').value;
+    socket.emit('message', text)
+}
+
+// Regular Websockets
+
+// const socket = new WebSocket('ws://localhost:8080');
+
+// // Listen for messages
+// socket.onmessage = ({ data }) => {
+//     console.log('Message from server ', data);
+// };
+
+// document.querySelector('button').onclick = () => {
+//     socket.send('hello');
+// }
